43cb5de46e6800a45b00454bdc8cb5e4ea26af84
Joern Berrisch <j.berrisch@gmail.com>
2015-09-17 16:08:04 +0200

    FIX: allow option value '0' (zero)

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ function Argv (processArgs, cwd) {
         
         var missing = [];
         Object.keys(demanded).forEach(function (key) {
-            if (!argv[key]) missing.push(key);
+            if (typeof argv[key] === 'undefined' || argv[key] === null) missing.push(key);
         });
         
         if (missing.length) {
